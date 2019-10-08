0c383308eb7f26606a664837e29721ea8ec38146
ntern.yangguo@outlook.com <yangguo2019+++>
2019-10-08 14:34:47 +1100

    Initial commit​

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lucas/Documents/Program_Project/Forum_Project/ForumSystem-master/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
